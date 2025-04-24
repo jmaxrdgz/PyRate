791b677b5007f0c177993802eb7077886680e365
Jean-Max Rodriguez <jmaxrdgz@gmail.com>
2025-04-24 14:26:05 +0200

    Add keyboard control

---
+++ pyrate/engine/input.py
@@ -0,0 +1,17 @@
+# pyrate/engine/input.py
+import pygame
+
+def handle_input(ship):
+    """
+    Allows to control a ship using the keyboard (Z, Q, S, D).
+    """
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_z]:
+        ship.accelerate()
+    if keys[pygame.K_s]:
+        ship.decelerate()
+    if keys[pygame.K_q]:
+        ship.turn_left()
+    if keys[pygame.K_d]:
+        ship.turn_right()
